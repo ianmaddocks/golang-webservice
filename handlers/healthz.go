da4349df655311f233cc791a4fcbf22d54882663
Ian Maddocks <37927672+ianmaddocks@users.noreply.github.com>
2022-05-14 08:31:38 +0530

    Update healthz.go

--- handlers/healthz.go
+++ handlers/healthz.go
@@ -1,8 +1,20 @@
 package handlers
 
-import "net/http"
+import {
+  "net/http"
+  "time"
+}
 
 // healthz is a liveness probe.
 func healthz(w http.ResponseWriter, _ *http.Request) {
-	w.WriteHeader(http.StatusOK)
+duration := time.Now().Sub(started)
+    if duration.Seconds() > 10 {
+        w.WriteHeader(500)
+        w.Write([]byte(fmt.Sprintf("error: %v", duration.Seconds())))
+    } else {
+        w.WriteHeader(http.StatusOK)
+        w.Write([]byte("ok"))
+    }
+
+	//w.WriteHeader(http.StatusOK)
 }
